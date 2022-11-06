d74b0d2def080f74f63c59fa4c0ef00a4aa07264
zhuzedan <1031155817@qq.com>
2022-11-06 12:27:50 +0800

    认证过滤器

--- src/main/java/com/zzd/authdemo/config/SecurityConfig.java
+++ src/main/java/com/zzd/authdemo/config/SecurityConfig.java
@@ -1,15 +1,18 @@
 package com.zzd.authdemo.config;
 
+import com.zzd.authdemo.filter.JwtAuthenticationTokenFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 public class SecurityConfig{
@@ -18,7 +21,8 @@ public class SecurityConfig{
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
     }
-
+    @Autowired
+    JwtAuthenticationTokenFilter jwtAuthenticationTokenFilter;
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
@@ -33,9 +37,12 @@ public class SecurityConfig{
                 // 除上面外的所有请求全部需要鉴权认证
                 .anyRequest().authenticated();
 
+        //把token校验过滤器添加到过滤器链中
+        http.addFilterBefore(jwtAuthenticationTokenFilter, UsernamePasswordAuthenticationFilter.class);
+
         return http.build();
     }
-
+    //放行
     @Autowired
     private AuthenticationConfiguration authenticationConfiguration;
 
